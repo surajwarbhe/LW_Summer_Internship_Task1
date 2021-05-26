ec71d63f022614d6c34bae172638ee8c128723cb
surajwarbhe <67333590+surajwarbhe@users.noreply.github.com>
2021-05-26 22:05:21 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import joblib
+
+db = pd.read_csv('Salary_Data.csv')
+x = db.['YearsExperience'].values.reshape(1,-1)
+y = db.['Salary']
+
+from sklearn.linear_model import LinearRegression
+model = LinearRegression()
+moodel.fit(x,y)
+joblib.dump(model,'marks.pk1')
