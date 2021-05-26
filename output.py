d92f14c59a54ea373d117519542b440fe842d0a3
surajwarbhe <67333590+surajwarbhe@users.noreply.github.com>
2021-05-27 00:16:21 +0530

    Update output.py

--- output.py
+++ output.py
@@ -4,7 +4,7 @@ name = input("Enter Employee Name		: ", name)
 exp = float(input("Enter Employee Experience	: ", name)
 model = joblib.load('marks.pk1')
 
-print("\n"
+print("\n")
 print("Name			: ", name)
 print("Year of Experience	: ", exp)
 sal = model.predict([[exp]])
